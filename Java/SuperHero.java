fcf371967c7e89c6b1edab0b5e9cc03987174009
atzh8djk <atzh8djk@yahoo.co.jp>
2017-11-19 12:28:59 +0100

    Comment added

--- Java/SuperHero.java
+++ Java/SuperHero.java
@@ -1,27 +1,27 @@
 /**
- * 
- * @author akira
  *
+ * @author akira
+ * ヒーローを継承してスーパーヒーロを作る
  */
 public class SuperHero extends Hero {
-	
+
 	/**
 	 * ヒーローの年と趣味
 	 */
 	private int age;
 	private String hobby;
-	
+
 	public SuperHero() {
-		this.age = 12;
+		this.age = 14;
 		this.hobby= "Game_Play";
-		
+
 		//super(0, "ここう",25000,5, "夜");
 	}
 
 	public SuperHero(int age, String hobby) {
 		this.age = age;
 		this.hobby= hobby;
-		
+
 		//super(0, "ここう",25000,5, "夜");
 	}
 	/**
@@ -30,9 +30,8 @@ public class SuperHero extends Hero {
 	public int getAge(){
 		return this.age;
 	}
-	
+
 	public String getHobby(){
 		return this.hobby;
 	}
 }
-
